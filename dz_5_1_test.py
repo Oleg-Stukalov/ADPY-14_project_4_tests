c04dc374ff035a22f5efbce9ffaf076cd615ff8f
Oleg Stukalov <oleg.stukalov@mail.ru>
2020-08-30 18:41:56 +0300

    draft yandex_disk_test added

--- dz_5_1_test.py
+++ dz_5_1_test.py
@@ -1,20 +1,42 @@
+import unittest
+from dz_5_1_functions import Functions_5_1
+
+class TestFunctions(unittest.TestCase):
+
+  #setUp method is overridden from the parent class TestCase
+  def setUp(self):
+    self.functions = Functions_5_1()
+
+  #Each test method starts with the keyword test_
+  def test_people(self):
+    self.assertEqual(self.functions.number2name('2207 876234'), 'Василий Гупкин')
+    self.assertEqual(self.functions.number2name('11-2'), 'Геннадий Покемонов')
+    self.assertEqual(self.functions.number2name('10006'), 'Аристарх Павлов')
+
+  def test_shelf(self):
+    self.assertEqual(self.functions.number2shelf('2207 876234'), '1')
+    self.assertEqual(self.functions.number2shelf('11-2'), '1')
+    self.assertEqual(self.functions.number2shelf('5455 028765'), '1')
+    self.assertEqual(self.functions.number2shelf('1006'), '2')
+
+  def test_list(self):
+    self.assertEqual(self.functions.list_documents('passport', '2207 876234', 'Василий Гупкин'), True)
+    self.assertEqual(self.functions.list_documents('invoice', '11-2', 'Геннадий Покемонов'), True)
+    self.assertEqual(self.functions.list_documents('insurance', '10006', 'Аристарх Павлов'), True)
+
+  def test_add(self):
+    self.assertEqual(self.functions.add_document('testdoc', '12345', 'Ivan Petrov', '1'), True)
+
+# Executing the tests in the above test case class
+if __name__ == "__main__":
+  unittest.main()
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-Задача №1 unit-tests
-
-Из курса «Python: программирование на каждый день и сверхбыстрое прототипирование» необходимо протестировать программу по работе с бухгалтерией Лекции 2.1. При наличии своего решения данной задачи можно использовать его или использовать предложенный код в директории scr текущего задания.
-
-    Следует протестировать основные функции по получению информации о документах, добавлении и удалении элементов из словаря.
-    Используйте fixture для загрузки данных в тестовый класс.
+# Задача №1 unit-tests
+#
+# Из курса «Python: программирование на каждый день и сверхбыстрое прототипирование» необходимо протестировать программу по работе с бухгалтерией Лекции 2.1. При наличии своего решения данной задачи можно использовать его или использовать предложенный код в директории scr текущего задания.
+#
+#     Следует протестировать основные функции по получению информации о документах, добавлении и удалении элементов из словаря.
+#     Используйте fixture для загрузки данных в тестовый класс.
---
+++ yandex_disk.py
@@ -0,0 +1,63 @@
+import unittest
+import requests
+
+#CONSTANTS
+YD_URL = 'https://cloud-api.yandex.net:443/v1/disk'
+YANDEX_UPLOAD_URL = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+##############!!!!!!!!!!!!!!!!!!!!
+TOKEN_YD = 'AgAAAAAy_UpkAADLW4DINWqLNUrUml_SsYrvX7U'
+#TOKEN_YD = input('Пожалуйста, введите токен учетной записи Яндекс, куда будем сохранять копию фото: ')
+YD_OAUTH = {'Authorization': f'OAuth {TOKEN_YD}'}
+yandex_oauth_header = {
+    'Accept': 'application/json',
+    'Authorization': f'OAuth {TOKEN_YD}'
+}
+print('Сохранен токен Яндекс: ', TOKEN_YD)
+
+
+class YDUser:
+    """
+    YDUser is a class that do all work with Yandex API
+    """
+
+    def put_request(self, url, params, headers):
+        response = requests.put(url, params=params, headers=headers)
+        return response.json()
+
+    def yandex_folder(self):
+        yandex_folder_url = f'{YD_URL}{"/resources"}'
+        # доп параметры для создания папки ЯД
+        yandex_folder_params = {
+            'path': f'{"id_VK-"}{id_VK}',
+            'overwrite': 'true'
+        }
+        response = self.put_request(yandex_folder_url, params=yandex_folder_params, headers=yandex_oauth_header)
+        return response
+
+    def yandex_upload(self, files_for_upload):
+        for file in files_for_upload:
+            # доп параметры для получения ссылки на загрузку файла
+            yandex_upload_params = {
+                'path': f'{"id_VK-"}{id_VK}{"/"}{file}',
+                'overwrite': 'true'
+            }
+            response = requests.get(YANDEX_UPLOAD_URL, params=yandex_upload_params, headers=yandex_oauth_header)
+            put_url = response.json().get('href')
+            # открыть файл на БИНАР чтение, передать его в яндекс!
+            with open(file, 'rb') as f:
+                data_4upload = f.read()
+            response_upload = requests.put(put_url, data=data_4upload)
+
+            # Прогресс бар upload
+            for i in tqdm(range(2)):
+                time.sleep(1)
+            print(f'Файл: "{file}" - успешно загружен!')
+            print()
+
+        return print(f'Все файлы надежно сохранены в Яндекс.диск!')
+
+user1 = YDUser()
+user1.yandex_folder()
+user1.yandex_upload(files)
+
+
